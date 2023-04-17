156ca3ad5ca5b83a70acb50007692cb0c9829f7f
martin korevaar <m.w.korevaar@gmail.com>
2023-04-17 12:33:11 +0200

    Let test fail instead of raise NotImplementedError

--- tests/test_io.py
+++ tests/test_io.py
@@ -5,7 +5,7 @@ import wadi as wd
 def test_correct_units():
     """ Test whether the to-hgc method is using the correct units and names
     of quantitties """
-    raise NotImplementedError()
+    assert False
     assert constants == wd.used_hgc_units()
 
 
