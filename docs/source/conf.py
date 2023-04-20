fe265d925403c32ef8fcc54fabf0bbf2fed8d538
martin korevaar <m.w.korevaar@gmail.com>
2023-04-20 20:25:44 +0200

    update version in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Vincent Post, Martin van der Schans, Martin Korevaar'
 author = 'Vincent Post, Martin van der Schans, Martin Korevaar'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0'
+release = '0.1'
 
 
 # -- General configuration ---------------------------------------------------
